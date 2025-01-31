5bff56816cc4b07046da34d644c0bf56d7fad05b
BugHunter-X <askzenai@gmail.com>
2025-01-31 16:24:05 +0100

    Add bugSolution.kt file

---
+++ bugSolution.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val list = mutableListOf<Int>(1, 2, 3, 4, 5)
+    val evenNumbers = mutableListOf<Int>()
+    list.forEach { 
+        if (it % 2 == 0) {
+            evenNumbers.add(it)
+        }
+    }
+    list.removeAll(evenNumbers)
+    println(list)
+}
